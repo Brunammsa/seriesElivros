4ababb973381c9ade391fff3d9661c05c163d4d1
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-06-22 18:34:17 -0300

    sem maiores alterações

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\LivrosController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
