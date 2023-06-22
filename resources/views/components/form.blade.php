24580a9392e8982cddf45f257ecea1fda716060d
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-06-22 18:29:36 -0300

    alterando visual, botões e cores

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,13 +1,12 @@
 <form action="{{ $action }}" method="post">
     @csrf
 
-    
     @if($update)
     @method('PUT')
     @endif
     
-    <div class="mb-3">
-        <label for="nome" class="form-label">Nome:</label>
+    <div class="mb-4">
+        <label for="nome" class="form-label mt-4 mb-4">Nome:</label>
         <input type="text"  
                 id="nome" 
                 name="nome" 
@@ -15,6 +14,6 @@
                 @isset($nome) value="{{ $nome }}"@endisset>
     </div>
     
-    <button type="submit" class="btn btn-outline-success btn-sm">Salvar</button>
-    <a role="button" class="btn btn-outline-success btn-sm" href="{{ route('dashboard') }}">Voltar</a>
+    <button type="submit" class="btn btn-outline-light btn-sm" style="color: #DB005B; border-color: #B70404;">Salvar</button>
+    <a role="button" class="btn btn-outline-light btn-sm" href="{{ route('dashboard') }}" style="color: #6a8020e7; border-color: #98a766e7;">Voltar</a>
 </form>
