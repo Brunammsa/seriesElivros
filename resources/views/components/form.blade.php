128b77594e7db28a3bb54c6917066a7fa47b5422
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-06-28 12:21:18 -0300

    atualizando 'nome'para 'name' e adicionando autofocus no input

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -5,15 +5,16 @@
     @method('PUT')
     @endif
     
-    <div class="mb-4">
-        <label for="nome" class="form-label mt-4 mb-4">Nome:</label>
-        <input type="text"  
-                id="nome" 
-                name="nome" 
-                class="form-control" 
-                @isset($nome) value="{{ $nome }}"@endisset>
+    <div class="mb-4 col-5">
+        <label for="name" class="form-label mt-4 mb-4">Novo nome:</label>
+        <input type="text"
+                autofocus  
+                id="name" 
+                name="name" 
+                class="form-control col-5" 
+                @isset($name) value="{{ $name }}"@endisset>
     </div>
     
     <button type="submit" class="btn btn-outline-light btn-sm" style="color: #DB005B; border-color: #B70404;">Salvar</button>
-    <a role="button" class="btn btn-outline-light btn-sm" href="{{ route('dashboard') }}" style="color: #6a8020e7; border-color: #98a766e7;">Voltar</a>
+    <a role="button" class="btn btn-outline-light btn-sm" href="{{ route('dashboard') }}" style="color: #6a8020e7; border-color: #98a766e7; margin-left: 1em">Voltar</a>
 </form>
