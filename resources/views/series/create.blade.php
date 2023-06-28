04a066ebba18cbeb2fac8dea0e6e89f363c83ad3
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-06-28 12:22:33 -0300

    utilizando o form dentro do create com atualizações de front

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,12 +1,23 @@
 <x-app-layout>
-    
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-12">
-                <label>Nova Série</label>
-                <x-form :action="route('series.store')" :nome="old('nome')" :update="false" />
-            </div>
-        </div>
+    <div class="container">
+        <h2 class="d-flex justify-content-center" style="background-color: #deeab6e7; color:#025464;">Nova série</h2>
     </div>
 
+    <form action="{{ route('series.store') }}" method="post">
+        @csrf
+
+        <div class="mb-4 col-3" style="margin-left: 20em">
+            <label for="name" class="form-label mt-4 mb-4">Nome:</label>
+            <input type="text" 
+                    autofocus 
+                    id="name" 
+                    name="name" 
+                    class="form-control" 
+                    value="{{ old('name') }}">
+            
+            <button type="submit" class="mt-4 btn btn-outline-light btn-sm" style="color: #DB005B; border-color: #B70404;">Salvar</button>
+            <a role="button" class="mt-4 btn btn-outline-light btn-sm" href="{{ route('dashboard') }}" style="color: #6a8020e7; border-color: #98a766e7; margin-left: 1em">Voltar</a>
+        </div>
+
+    </form>
 </x-app-layout>
