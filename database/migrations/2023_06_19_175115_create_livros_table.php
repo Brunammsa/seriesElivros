c8fd503c73a29557ed773ecbd3fbbfe6ff80d271
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-06-29 15:37:12 -0300

    adicionando coluca de assistido e lido nas tabelas serie e livro

--- database/migrations/2023_06_19_175115_create_livros_table.php
+++ database/migrations/2023_06_19_175115_create_livros_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('livros', function (Blueprint $table) {
             $table->id();
             $table->string('name', 255);
+            $table->boolean('watched')->default(false);
             $table->timestamps();
         });
     }
