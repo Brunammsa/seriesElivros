2cef0bcf808f17f724f41799051c1948fc6612a2
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-05 17:12:36 -0300

    tentativa de entender o erro do fecth api, no momento está como 404

--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -32,51 +32,54 @@
                 </div>
                 @endisset
 
-                <form action="/series" method="get">
-                    <ul class="list-group list-group-numbered list-group-flush mt-3">
-                        @foreach ($series as $serie)
-                        <li class="list-group-item d-flex align-items-center px-3" style="gap: 20px">
-                            <div class="me-auto p-2 bd-highlight">
-                                {{ $serie->name }}
-                            </div>       
-    
-                            <div class="d-flex align-items-center p-2 bd-highlight" style="gap: 45px">
-                                <input class="checkbox" type="checkbox" id="{{ $serie->id }}">
-    
-                                <a href="{{route('series.edit', $serie->id)}}" class="btn btn-sm">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
-                                        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                                        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                                    </svg>
-                                </a>
-            
-                                <form action="{{route('series.destroy', $serie->id)}}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn-close" aria-label="Close"></button>
-                                </form>
-                            </div>
-                        </li>
-                        @endforeach
-                    </ul>
-                </form>
+                <ul class="list-group list-group-numbered list-group-flush mt-3">
+                    @foreach ($series as $serie)
+                    <li class="list-group-item d-flex align-items-center px-3" style="gap: 20px">
+                        <div class="me-auto p-2 bd-highlight">
+                            {{ $serie->name }}
+                        </div>       
+
+                        <div class="d-flex align-items-center p-2 bd-highlight" style="gap: 45px">
+                            <input class="checkbox" type="checkbox" id="{{ $serie->id }}">
+
+                            <a href="{{route('series.edit', $serie->id)}}" class="btn btn-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
+                                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                                </svg>
+                            </a>
+        
+                            <form action="{{route('series.destroy', $serie->id)}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn-close" aria-label="Close"></button>
+                            </form>
+                        </div>
+                    </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
 
     <script>
         let checkboxes = document.getElementsByClassName('checkbox');
-    
-        function marcarComoLido(serieId) {
-            alert(`Checkbox do livro com ID ${serieId} foi clicado`);
-        }
 
         for (let checkbox of checkboxes) {
             checkbox.addEventListener('click', function() {
-                marcarComoLido(this.id);
-            });
-        }
-    
+                fetch(`/api/toggle/${this.id}/toggle-done`, {
+                    method:'put'
+                }).then(response => {
+                    if (!response.ok) {
+                        throw new Error('Erro na solicitação');
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+                    });
+                }
+
     </script>
 
 </x-app-layout>
