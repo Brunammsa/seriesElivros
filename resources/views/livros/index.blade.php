399dd4e83a34ebce6c1fad1c79259a194f415736
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-05 17:11:55 -0300

    incrementando js com fecth api incorreto

--- resources/views/livros/index.blade.php
+++ resources/views/livros/index.blade.php
@@ -32,36 +32,45 @@
                 </div>
                 @endisset
 
-                <form action="/livros" method="get">
-                    <ul class="list-group list-group-numbered list-group-flush mt-3">
-                        @foreach ($livros as $livro)
-                        <li class="list-group-item d-flex align-items-center px-3" style="gap: 20px">
-                            <div class="me-auto p-2 bd-highlight">
-                                {{ $livro->name }}
-                            </div>
+                <ul class="list-group list-group-numbered list-group-flush mt-3">
+                    @foreach ($livros as $livro)
+                    <li class="list-group-item d-flex align-items-center px-3" style="gap: 20px">
+                        <div class="me-auto p-2 bd-highlight">
+                            {{ $livro->name }}
+                        </div>
 
-                            <div class="d-flex align-items-center p-2 bd-highlight" style="gap: 45px;">
-                                <input class="form-check-input" type="checkbox" id="checkboxNoLabel">
+                        <div class="d-flex align-items-center p-2 bd-highlight" style="gap: 45px;">
+                            <input class="checkbox" type="checkbox" id="{{ $livros->id }}">
 
-                                <a href="{{route('livros.edit', $livro->id)}}" class="btn btn-sm">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
-                                        <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                                        <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                                    </svg>
-                                </a>
-            
-                                <form action="{{route('livros.destroy', $livro->id)}}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn-close" aria-label="Close"></button>
-                                </form>
-                            </div>
-                        </li>
-                        @endforeach
-                    </ul>
-                </form>
+                            <a href="{{route('livros.edit', $livro->id)}}" class="btn btn-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit">
+                                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                                </svg>
+                            </a>
+        
+                            <form action="{{route('livros.destroy', $livro->id)}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn-close" aria-label="Close"></button>
+                            </form>
+                        </div>
+                    </li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </div>
 
+    <script>
+        let checkboxes = document.getElementsByClassName('checkbox');
+
+        for (let checkbox of checkboxes) {
+            checkbox.addEventListener('click', function() {
+                fetch(`/api/toggle/${this.id}/toggle-done`, {
+                    method:'post'
+                });
+            });
+        }
+    </script>
 </x-app-layout>
