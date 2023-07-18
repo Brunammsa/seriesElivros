1b3b3f0b9cd319f25d6909e0fb985602f551aaff
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-18 09:14:06 -0300

    modificação para adicionar qnt temporadas e episódios

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -6,15 +6,37 @@
     <form action="{{ route('series.store') }}" method="post">
         @csrf
 
-        <div class="mb-4 col-3" style="margin-left: 20em">
-            <label for="name" class="form-label mt-4 mb-4">Nome:</label>
-            <input type="text" 
-                    autofocus 
-                    id="name" 
-                    name="name" 
-                    class="form-control" 
-                    value="{{ old('name') }}">
-            
+        <div class="row mb-3">
+            <div class="col-4" style="margin-left: 20em">
+                <label for="name" class="form-label mt-4 mb-4">Nome:</label>
+                <input type="text" 
+                        autofocus 
+                        id="name" 
+                        name="name" 
+                        class="form-control" 
+                        value="{{ old('name') }}">
+            </div>       
+            <div class="col-1" >
+                <label for="qntTemp" class="form-label mt-4 mb-4">N. Temporada:</label>
+                 
+                <input type="number" 
+                        autofocus 
+                        id='qntTemp' 
+                        name='qntTemp' 
+                        class="form-control" 
+                        value="{{ old('qntTemp') }}">
+            </div>
+            <div class="col-1" >
+                <label for="qntEps" class="form-label mt-4 mb-4">Episódio/Temporada</label>
+                <input type="number" 
+                        autofocus 
+                        id='qntEps' 
+                        name='qntEps' 
+                        class="form-control" 
+                        value="{{ old('qntEps') }}">
+            </div>
+        </div>
+        <div class="container">
             <button type="submit" class="mt-4 btn btn-outline-light btn-sm" style="color: #DB005B; border-color: #B70404;">Salvar</button>
             <a role="button" class="mt-4 btn btn-outline-light btn-sm" href="{{ route('dashboard') }}" style="color: #6a8020e7; border-color: #98a766e7; margin-left: 1em">Voltar</a>
         </div>
