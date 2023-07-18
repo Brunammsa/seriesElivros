8a25d98b7cc6e64f1c613f8dafbd6e039b5dec87
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-18 11:45:25 -0300

    recebendo como parâmetro o id da série, percorrendo a query da model tempodara onde o id recebido esteja relacionado a série_id

--- app/Http/Controllers/TemporadasController.php
+++ app/Http/Controllers/TemporadasController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Serie;
+use App\Models\Temporadas;
 
 class TemporadasController extends Controller
 {
-    public function index(Serie $serie, int $id)
+    public function index(int $id)
     {
-        $temporadas = Serie::find($id)->temporadas;
-        // $temporadas = $serie->temporadas()->with('episodios')->get();
+        $temporadas = Temporadas::where('series_id', $id)->get();
 
-        return view('temporadas.index')->with('temporadas', $temporadas)->with('serie', $serie);
+        return view('temporadas.index')->with('temporadas', $temporadas);
     }
 }
