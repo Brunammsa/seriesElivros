3614e6a36088e82cfca0ea329b6880cdc1c88844
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-24 14:42:50 -0300

    acrescentando a rota do mail

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Mail\SeriesCreated;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,4 +28,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
+
+Route::get('/email', function() {
+    return new SeriesCreated('Teste', 4, 2, 2);
+});
+
 require __DIR__.'/auth.php';
