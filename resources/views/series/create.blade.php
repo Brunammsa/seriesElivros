f4e00bd433790aabcbeffadcfebd9f4acbd94ca7
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-25 14:23:31 -0300

    adicionando label/input para capa

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -3,7 +3,7 @@
         <h2 class="d-flex justify-content-center" style="background-color: #deeab6e7; color:#025464;">Nova série</h2>
     </div>
 
-    <form action="{{ route('series.store') }}" method="post">
+    <form action="{{ route('series.store') }}" method="post" enctype="multipart/form-data">
         @csrf
 
         <div class="row mb-3">
@@ -37,9 +37,16 @@
             </div>
         </div>
         <div class="container">
+            <div class="row mb-3 mt-4">
+                <div class="col-5">
+                    <label for="cover" class="form-label">Capa</label>
+                    <input type="file" id="cover" name="cover" class="form-control" accept="image/gif, image/jpeg, image/png">
+                </div>
+            </div>
+        </div>
+        <div class="container">
             <button type="submit" class="mt-4 btn btn-outline-light btn-sm" style="color: #DB005B; border-color: #B70404;">Salvar</button>
             <a role="button" class="mt-4 btn btn-outline-light btn-sm" href="{{ route('dashboard') }}" style="color: #6a8020e7; border-color: #98a766e7; margin-left: 1em">Voltar</a>
         </div>
-
     </form>
 </x-app-layout>
