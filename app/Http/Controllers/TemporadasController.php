1c01c77554bd8cb2329c1a8a26249d44a10072e5
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-25 14:39:10 -0300

    realizando query de série para recuperar nome e capa através de variável

--- app/Http/Controllers/TemporadasController.php
+++ app/Http/Controllers/TemporadasController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use App\Models\Temporadas;
 
 class TemporadasController extends Controller
@@ -9,7 +10,8 @@ class TemporadasController extends Controller
     public function index(int $id)
     {
         $temporadas = Temporadas::with('episodios')->where('series_id', $id)->get();
+        $serie = Serie::where('id', $id)->select('name', 'cover')->first();
 
-        return view('temporadas.index')->with('temporadas', $temporadas);
+        return view('temporadas.index')->with('temporadas', $temporadas)->with('serie', $serie);
     }
 }
