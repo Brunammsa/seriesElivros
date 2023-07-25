2ec87ff07a07a38ffcbd0396c69b13b452790687
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-25 14:24:19 -0300

    adicionando título com nome da série e capa

--- resources/views/temporadas/index.blade.php
+++ resources/views/temporadas/index.blade.php
@@ -1,7 +1,18 @@
 <x-app-layout>
+    <div class="container">
+        <div>
+            <h1>Temporadas de '{{$serie->name}}'</h1>
+        </div>
+        <div class="d-flex justify-content-center">
+            <img src="{{asset('storage/' . $serie->cover )}}" 
+                alt="capa da série" 
+                class=" img-fluid" 
+                style="height: 400px">
+        </div>
+    </div>
     <div class="container d-flex justify-content-between">
-        <h2 class="mt-5">Temporadas</h2>
-        <h2 class="mt-5">Episódios</h2>
+        <h3 class="mt-5">Temporadas</h3>
+        <h3 class="mt-5">Episódios</h3>
     </div>
     <div class="container mt-5">
         <ul class="list-group list-group-flush">
@@ -19,5 +30,4 @@
             @endforeach
         </ul>
     </div>
-    
 </x-app-layout>
