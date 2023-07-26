0567ba4350958c0a7f9db0441e9628a51c7afbec
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-26 10:53:40 -0300

    nova pasta para api e seus arquivos

---
+++ app/Http/Controllers/Api/ApiCheckBoxController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Serie;
+use Illuminate\Http\Response;
+
+class ApiCheckBoxController extends Controller
+{
+    public function toggle(Serie $serie): Response
+    {
+        $isDone = $serie->read;
+
+        $text = 'marcado com sucesso'; 
+
+        if ($isDone) {
+            $text = 'desmarcado com sucesso';
+        };
+
+        $serie->read = !$serie->read;
+        $serie->save();
+        
+        return response($text, 200);
+    }
+}
