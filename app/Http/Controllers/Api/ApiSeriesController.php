d427e9d167265b3620aa82ca8ee6f5c61c25b0b4
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-27 18:10:44 -0300

    modificando método store com repositorio/seriesform e json, criação dos métodos show, destroy e update

--- app/Http/Controllers/Api/ApiSeriesController.php
+++ app/Http/Controllers/Api/ApiSeriesController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Serie;
+use App\Repositories\SeriesRepositoryInterface;
 
 class ApiSeriesController extends Controller
 {
+    public function __construct(private SeriesRepositoryInterface $seriesRepository)
+    {
+    }
     public function index()
     {
         return Serie::all();
@@ -15,6 +19,36 @@ class ApiSeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        return response()->json(Serie::create($request->all()), 201);
+        return response()
+            ->json(
+                $this->seriesRepository->add($request),
+                201
+            );
+    }
+
+    public function show(int $serie)
+    {
+        $serie = Serie::with('temporadas.episodios')->find($serie);
+
+        if (is_null($serie)) {
+            return response()->json(['série não encontrada'], 404);
+        }
+
+        return $serie;
+    }
+
+    public function destroy(Serie $series)
+    {
+        $series->delete();
+
+        return response()->json(["série $series->name removida"], 200);
+    }
+
+    public function update(Serie $series, SeriesFormRequest $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return $series;
     }
 }
