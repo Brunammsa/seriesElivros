b1e3d8b924f501eb8650a3252904ef4cb49863df
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-30 14:54:11 -0300

    utilizando sanctum

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Api\ApiCheckBoxController;
 use App\Http\Controllers\Api\ApiTemporadasController;
 use App\Http\Controllers\Api\ApiEpisodiosController;
 use App\Http\Controllers\Api\ApiUploadController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,16 +26,35 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:sanctum')->group(function (){
+    Route::apiResource('/series', ApiSeriesController::class);
+
+    Route::post('/upload', [ApiUploadController::class, 'upload']);
+    Route::get('/series/{serie}/temporadas', [ApiTemporadasController::class, 'show']);
+
+    Route::get('/series/{serie}/episodios', [ApiEpisodiosController::class, 'show']);
+
+    Route::patch('episodios/{episodio}', [ApiEpisodiosController::class, 'update']);
+});
+
 Route::controller(ApiCheckBoxController::class)->group(function(){
     Route::post('toggle/{serie}/toggle-done', 'toggle');
 });
 
-Route::apiResource('/series', ApiSeriesController::class);
+Route::post('/login', function(Request $request){
+    $credenciais = $request->only(['email', 'password']);
+    if (!Auth::attempt($credenciais)) {
+        return response()->json('Unauthorized', 401);
+    }
+    // $user = User::whereEmail($credenciais['email'])
+    //     ->first();
 
-Route::post('/upload', [ApiUploadController::class, 'upload']);
-Route::get('/series/{serie}/temporadas', [ApiTemporadasController::class, 'show']);
-
-Route::get('/series/{serie}/episodios', [ApiEpisodiosController::class, 'show']);
-
-Route::patch('episodios/{episodio}', [ApiEpisodiosController::class, 'update']);
-
+    // if (is_null($user) || !Hash::check($credenciais['password'], $user->password)) {
+    //     return response()->json('Unauthorized', 401);
+    // };
+    $user = Auth::user();
+    $user->tokens()->delete();
+    $token = $user->createToken('token');
+    
+    return response()->json($token->plainTextToken);
+});
