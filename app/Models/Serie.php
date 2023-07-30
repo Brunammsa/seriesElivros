d7024f058561b2c5c4f3971ca3f8fb81bb98f918
Bruna Sa <bruna@Mac-mini-de-Bruna.local>
2023-07-30 13:51:03 -0300

    incrementando a model serie accessor para link

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,8 @@ class Serie extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'read', 'cover'];
+    protected $appends = ['links'];
+
 
     public function temporadas()
     {
@@ -26,5 +29,25 @@ class Serie extends Model
         self::addGlobalScope('ordered', function(Builder $queryBuilder) {
             $queryBuilder->orderBy('name');
         });
+    }
+
+    public function links(): Attribute
+    {
+        return new Attribute(
+            get: fn() => [
+                [
+                    'rel' => 'self',
+                    'url' => "/api/series/{$this->id}"
+                ],
+                [
+                    'rel' => 'temporadas',
+                    'url' => "/api/series/{$this->id}/temporadas"
+                ],
+                [
+                    'rel' => 'episodios',
+                    'url' => "/api/series/{$this->id}/episodios"
+                ],
+            ],
+        );
     }
 }
